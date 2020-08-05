20b6925b3f955ccc20dc3a32ddf3143e3658a800
jubrother <jubrother@naver.com>
2020-08-05 09:28:21 +0900

    merge

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,13 +9,13 @@
 </template>
 <script>
 import "./components/css/style.scss";
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import Vue from 'vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// // import 'bootstrap/dist/css/bootstrap.css'
+// // import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+// Vue.use(BootstrapVue)
+// Vue.use(IconsPlugin)
 
 export default {
   name: "app"
