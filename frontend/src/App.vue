30c8fddb58b6cd608028f6192de612ace8c5f39b
yeonhee <dhfbsk1@naver.com>
2020-08-10 13:25:27 +0900

    resolve conflict

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <v-app id="app">
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -7,15 +6,6 @@
     </head>
     <router-view></router-view>
   </v-app>
-=======
-<div id="app">
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>코스모스</title>
-  </head>
-  <router-view></router-view>
-</div>
->>>>>>> 471e742f91fb5a43df580f71f496c732d973b999
 </template>
 
 <script>
