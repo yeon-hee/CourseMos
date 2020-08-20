2d363731ddc71ad851705554719deec86a6d5458
jubrother <jubrother@naver.com>
2020-08-20 13:46:18 +0900

    engToKor

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,103 +1,95 @@
 <template>
-    <div>
-        <v-card
-    class="mx-auto"
-    max-width="500"
-    style="padding-bottom: 60px;"
-  >
-    <v-sheet class="pa-4" style="background-color: rgb(245,152,152); ">
-      <v-text-field
-        v-model="search"
-        label="유저 검색"
-        dark
-        flat
-        solo-inverted
-        hide-details
-        clearable
-        clear-icon="mdi-close-circle-outline"
-        style="background-color: rgb(245,152,152);"
-      ></v-text-field>
-      <v-checkbox
-        v-model="caseSensitive"
-        dark
-        hide-details
-        label="Case sensitive search"
-      ></v-checkbox>
-    </v-sheet><br>
-        <v-treeview
-            :active.sync="active"
-            :items="items"
-            :search="search"
-            :filter="filter"
-            :open.sync="open"
-            transition
-            activatable
-            v-model="selection"
-            @update:active="onChange"
-        >
+  <div>
+    <v-card class="mx-auto" max-width="500" style="padding-bottom: 60px;">
+      <v-sheet class="pa-4" style="background-color: rgb(245,152,152); ">
+        <v-text-field
+          v-model="search"
+          label="유저 검색"
+          dark
+          flat
+          solo-inverted
+          hide-details
+          clearable
+          clear-icon="mdi-close-circle-outline"
+          style="background-color: rgb(245,152,152);"
+        ></v-text-field>
+        <v-checkbox v-model="caseSensitive" dark hide-details label="대소문자 구별하여 검색"></v-checkbox>
+      </v-sheet>
+      <br />
+      <v-treeview
+        :active.sync="active"
+        :items="items"
+        :search="search"
+        :filter="filter"
+        :open.sync="open"
+        transition
+        activatable
+        v-model="selection"
+        @update:active="onChange"
+      >
         <template v-slot:prepend="{ item }">
-            <v-icon v-if="!item.children">mdi-account</v-icon> {{ selected.name }}
+          <v-icon v-if="!item.children">mdi-account</v-icon>
+          {{ selected.name }}
         </template>
       </v-treeview>
-  </v-card>
-    </div>
+    </v-card>
+  </div>
 </template>
 
 <script>
 import LogoTitle from "./LogoTitle.vue";
 import Nav from "./Nav.vue";
 import UserApi from "../api/UserApi";
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 export default {
-    components: {
-        // LogoTitle,
-        // Nav
-    },
-    methods : {
-        onChange(selection) {
-            console.log('들어옴!!');
-            console.log(selection[0]);
-            var userId = null;
+  components: {
+    // LogoTitle,
+    // Nav
+  },
+  methods: {
+    onChange(selection) {
+      console.log("들어옴!!");
+      console.log(selection[0]);
+      var userId = null;
 
-            for(var i=0;i<this.items.length;i++){
-                if(this.items[i].id === selection[0]) {
-                    userId = this.items[i].name;
-                    break;
-                }
-            }
-            console.log(userId);
-            if(userId == localStorage.getItem('userId'))
-                this.$router.push("/users/profile");
-            else 
-                this.$router.push("/users/profile/info/" + userId);
+      for (var i = 0; i < this.items.length; i++) {
+        if (this.items[i].id === selection[0]) {
+          userId = this.items[i].name;
+          break;
         }
+      }
+      console.log(userId);
+      if (userId == localStorage.getItem("userId"))
+        this.$router.push("/users/profile");
+      else this.$router.push("/users/profile/info/" + userId);
     },
-    watch : {
-        keyword: function(v) {
-            if(v.length > 0) {
-                UserApi.findUserBykeyword(
-                    v,
-                    response => {
-                        this.users = response.data
-                    },
-                    error => {
-                        alert('유저 목록 조회에 실패했습니다.');
-                    }
-                );
-            } else {
-                this.users = []
-            }
-        },
-        selected: 'randomAvatar',
+  },
+  watch: {
+    keyword: function (v) {
+      if (v.length > 0) {
+        UserApi.findUserBykeyword(
+          v,
+          (response) => {
+            this.users = response.data;
+          },
+          (error) => {
+            alert("유저 목록 조회에 실패했습니다.");
+          }
+        );
+      } else {
+        this.users = [];
+      }
     },
-    data: () => {
-    return { 
-        keyword : "",
-        users : [],
-        items: [],
-        active: [],
-      temp : [],
+    selected: "randomAvatar",
+  },
+  data: () => {
+    return {
+      keyword: "",
+      users: [],
+      items: [],
+      active: [],
+      temp: [],
       open: [1, 2],
       search: null,
       caseSensitive: false,
@@ -105,46 +97,45 @@ export default {
   },
 
   created() {
-        let data = {
-            token : localStorage.getItem('token'),
-        };
+    let data = {
+      token: localStorage.getItem("token"),
+    };
 
-        UserApi.findAllUser(
-            data,
-            res => {
-                this.temp = res.data;
-                for(var i=0;i<this.temp.length;i++){
-                    this.items.push({
-                        id: i+1,
-                        name: this.temp[i].userId});
-                }
-                console.log(this.items);
-            },
-            error => {
-                alert(error);
-            }
-        );
-    },
+    UserApi.findAllUser(
+      data,
+      (res) => {
+        this.temp = res.data;
+        for (var i = 0; i < this.temp.length; i++) {
+          this.items.push({
+            id: i + 1,
+            name: this.temp[i].userId,
+          });
+        }
+        console.log(this.items);
+      },
+      (error) => {
+        alert(error);
+      }
+    );
+  },
 
   computed: {
-       filter () {
-        return this.caseSensitive
-          ? (item, search, textKey) => item[textKey].indexOf(search) > -1
-          : undefined
-      },
-      selected () { // 여기 눌리면 오는 곳 
-          return '안녕하세요';
-      },
-  }
-
-}
+    filter() {
+      return this.caseSensitive
+        ? (item, search, textKey) => item[textKey].indexOf(search) > -1
+        : undefined;
+    },
+    selected() {
+      // 여기 눌리면 오는 곳
+      return "안녕하세요";
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-.resultContainer > select{
-    padding-left: 5px;
-    width: 100%;
+.resultContainer > select {
+  padding-left: 5px;
+  width: 100%;
 }
-
 </style>
